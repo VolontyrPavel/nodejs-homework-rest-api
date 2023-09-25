be4fb695bc74173a3fffada3c4db40a08a5a2aa5
Avtark <103372792+VolontyrPavel@users.noreply.github.com>
2023-09-25 15:39:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const authRouter = require("./routes/api/auth");
+const authRouter = require("./routes/api/user");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -15,7 +15,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
+app.use("/api/user", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
