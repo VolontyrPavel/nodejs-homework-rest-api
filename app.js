eb024b3495a666aed266ee74eeb07550854e1dbd
Avtark <103372792+VolontyrPavel@users.noreply.github.com>
2023-09-27 21:52:04 +0300

    made

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const authRouter = require("./routes/api/users");
+const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -14,8 +14,9 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use("/api/users", authRouter);
+app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,20 +2,22 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 
 const {
-  addUserSchema,
+  addUserValidationSchema,
 } = require("../../utils/validation/userValidationSchema");
 
 const router = express.Router();
 
-router.post("/register", validateBody(addUserSchema), ctrl.register);
+router.post("/register", validateBody(addUserValidationSchema), ctrl.register);
 
-router.post("/login", validateBody(addUserSchema), ctrl.login);
+router.post("/login", validateBody(addUserValidationSchema), ctrl.login);
 
 router.post("/logout", authenticate, ctrl.logout);
 
 router.get("/current", authenticate, ctrl.current);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 module.exports = router;
